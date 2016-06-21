1a3dcb6a7434bd421a64a1b7feba0a923b5a9d43
Titwin <thibault.laine@inria.fr>
2016-06-21 02:24:39 +0200

    Added a clean base for my personnal applications
    Please don't modify this file without my permission, please !
    If you need your own app duplicate this one and change its name.

---
+++ Sources/GolemFactoryApplicationThibault.cpp
@@ -0,0 +1,72 @@
+// Golem Factory 1.0.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "Utiles\System.h"
+
+GLFWwindow* initGLFW();
+void initGLEW(int verbose = 1);
+
+static void errorCallback(int error, const char* description) { std::cerr << "GLFW ERROR : " << description << std::endl; }
+
+
+int main()
+{
+	GLFWwindow* window = initGLFW();
+	initGLEW();
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
+
+
+GLFWwindow* initGLFW()
+{
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+
+	glfwSetErrorCallback(errorCallback);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	GLFWwindow*window = glfwCreateWindow(640, 480, "Golem Factory v1.0", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+	return window;
+}
+void initGLEW(int verbose)
+{
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (GLEW_OK != err)
+	{
+		std::cerr << "ERROR : " << glewGetErrorString(err) << std::endl;
+		glfwTerminate();
+		exit(-1);
+	}
+	std::cout << "GLEW init success" << std::endl;
+	if (verbose < 1) return;
+
+	std::cout << "Status: GLEW version : " << glewGetString(GLEW_VERSION) << std::endl;
+	std::cout << "        OpenGL version : " << glGetString(GL_VERSION) << std::endl;
+	std::cout << "        OpenGL implementation vendor : " << glGetString(GL_VENDOR) << std::endl;
+	std::cout << "        Renderer name : " << glGetString(GL_RENDERER) << std::endl;
+	std::cout << "        GLSL version : " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+}
