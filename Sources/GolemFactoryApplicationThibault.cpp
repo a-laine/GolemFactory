3a367297e3f6e88c060c7c2afa8b4e3feaa033db
Titwin <thibault.laine@inria.fr>
2018-01-06 01:27:46 +0100

    prepare scenemanager for clean query system

--- Sources/GolemFactoryApplicationThibault.cpp
+++ Sources/GolemFactoryApplicationThibault.cpp
@@ -61,72 +61,16 @@ int main()
 	initManagers();
 
 	//	Test scene
-		//initializeForestScene(false);
+		Renderer::getInstance()->setShader(Renderer::GRID, ResourceManager::getInstance()->getShader("wired"));
+		initializeForestScene(false);
 
 		avatar = InstanceManager::getInstance()->getInstanceAnimatable("peasant", "human", "simple_peasant", "skinning");
 			float scale = 1.7f / (avatar->getBBMax() - avatar->getBBMin()).z;
 			avatar->setSize(glm::vec3(scale));
 			avatar->setPosition(glm::vec3(20.f, 20.f, -scale * avatar->getMesh()->aabb_min.z));
 			SceneManager::getInstance()->addObject(avatar);
-
 			camera.setMode(Camera::TRACKBALL);
 			camera.setRadius(4);
-			Renderer::getInstance()->setShader(Renderer::GRID, ResourceManager::getInstance()->getShader("wired"));
-
-
-
-
-
-	//	test instance
-	InstanceDrawable* bigTree = InstanceManager::getInstance()->getInstanceDrawable();
-	bigTree->setMesh("firTree1.obj");
-	bigTree->setShader("default");
-	
-	//	1
-	const unsigned int TESTNB = 30000;
-	InstanceDrawable* array[TESTNB];
-	glm::vec3 speed[TESTNB];
-	srand(0);
-	double startTestTime = glfwGetTime();
-	for (unsigned int i = 0; i < TESTNB; ++i)
-	{
-		array[i] = InstanceManager::getInstance()->getInstanceDrawable("firTree1.obj");
-		array[i]->setPosition(glm::vec3(((float)(rand() % TESTNB) / TESTNB - 0.5f) * (GRID_SIZE - 1)*GRID_ELEMENT_SIZE, ((float)(rand() % TESTNB) / TESTNB - 0.5f) * (GRID_SIZE - 1)*GRID_ELEMENT_SIZE, 0.f));
-		array[i]->setSize(glm::vec3((float)(rand() % TESTNB) / TESTNB + 1.7f));
-		speed[i] = 20.f * glm::vec3(((float)(rand() % TESTNB) / TESTNB - 0.5f), ((float)(rand() % TESTNB) / TESTNB - 0.5f), 0.f);
-		if(!SceneManager::getInstance()->addObject(array[i])) std::cout << '*';
-		if (i % (TESTNB/100) == 0) std::cout << '.';
-	}
-	double testTime = glfwGetTime() - startTestTime;
-	std::cout << std::endl << "create & insert (" << SceneManager::getInstance()->getNumberInstanceStored() << ") : " << testTime << 's' << std::endl;
-
-	//	2
-	const unsigned int UPDATE = 3000;
-	startTestTime = glfwGetTime();
-	for (unsigned int j = 0; j < UPDATE; ++j)
-	{
-		for (unsigned int i = 0; i < TESTNB; ++i)
-		{
-			array[i]->setPosition(array[i]->getPosition() + 0.016f * speed[i]);
-			SceneManager::getInstance()->updateObject(array[i]);
-			//if (i % (TESTNB / 100) == 0) std::cout << '.';
-		}
-		std::cout << '.';
-	}
-	testTime = glfwGetTime() - startTestTime;
-	std::cout << std::endl << "update (" << SceneManager::getInstance()->getNumberInstanceStored() << ") : " << testTime << 's' << std::endl;
-
-	//	3
-	startTestTime = glfwGetTime();
-	for (unsigned int i = 0; i < TESTNB; ++i)
-	{
-		SceneManager::getInstance()->removeObject(array[i]);
-		InstanceManager::getInstance()->release(array[i]);
-		if (i % (TESTNB / 100) == 0) std::cout << '.';
-	}
-	testTime = glfwGetTime() - startTestTime;
-	std::cout << std::endl << "deletion (" << SceneManager::getInstance()->getNumberInstanceStored() << ") : " << testTime << 's' << std::endl;
-	exit(0);
 
 	// init loop time tracking
 	double elapseTime = 16.;
@@ -148,7 +92,6 @@ int main()
 		Renderer::getInstance()->render(&camera);
 		picking(width, height);
 		glm::mat4 projection = glm::perspective(glm::radians(camera.getFrustrumAngleVertical()), (float)width / height, 0.1f, 1500.f);
-		Renderer::getInstance()->drawInstanceAnimatable(avatar, &camera.getViewMatrix()[0][0], &projection[0][0]);
 		Renderer::getInstance()->renderHUD(&camera2);
 
 		//  handle events
@@ -482,7 +425,7 @@ void events()
 void updates(float elapseTime, int width, int height)
 {
 	//	animate avatar
-	avatar->animate(elapseTime*0.1f);
+	avatar->animate(elapseTime);
 
 	//	Compute HUD picking parameters
 	if (EventHandler::getInstance()->getCursorMode())
@@ -498,7 +441,6 @@ void updates(float elapseTime, int width, int height)
 	{
 		WidgetManager::getInstance()->setPickingParameters(glm::mat4(1.f), glm::vec3(0.f), camera.getPosition());
 	}
-
 
 	//	Update widgets
 	averageCompleteTime = 0.99f * averageCompleteTime + 0.01f * completeTime;
@@ -519,6 +461,7 @@ void updates(float elapseTime, int width, int height)
 		if (avatar->isAnimationRunning("run")) speed = 0.1f;
 		avatar->setPosition(avatar->getPosition() + speed * glm::normalize(glm::vec3(forward.x, forward.y, 0.f)));
 		if (speed != 0.f) avatar->setOrientation(glm::rotate(glm::pi<float>() / 2.f + atan2(forward.y, forward.x), glm::vec3(0.f, 0.f, 1.f)));
+		SceneManager::getInstance()->updateObject(avatar);
 	}
 
 	//Animate camera
