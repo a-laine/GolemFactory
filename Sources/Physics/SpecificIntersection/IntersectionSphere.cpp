a0b2ca1555168795bb81cdf95a00a0536bec75f2
Unknown <titwin@live.fr>
2019-07-30 20:58:12 +0200

    more implementation
    - fix missing include
    - implement sphere vs sphere
    - implement sphere vs capsule

--- Sources/Physics/SpecificIntersection/IntersectionSphere.cpp
+++ Sources/Physics/SpecificIntersection/IntersectionSphere.cpp
@@ -1,11 +1,31 @@
 #include "IntersectionSphere.h"
-
+#include "Physics/SpecificCollision/CollisionUtils.h"
 
 Intersection::Contact Intersection::intersect_SpherevsSphere(const glm::vec3& sphere1Center, const float& sphere1Radius, const glm::vec3& sphere2Center, const float& sphere2Radius)
 {
-	return Intersection::Contact();
+	glm::vec3 n = glm::normalize(sphere2Center - sphere1Center);
+	return Intersection::Contact(sphere1Center + sphere1Radius * n, sphere2Center - sphere2Radius * n, n, -n);
 }
 Intersection::Contact Intersection::intersect_SpherevsCapsule(const glm::vec3& sphereCenter, const float& sphereRadius, const glm::vec3& capsule1, const glm::vec3& capsule2, const float& capsuleRadius)
 {
-	return Intersection::Contact();
+	Contact contact;
+	contact.contactPointB = getSegmentClosestPoint(capsule1, capsule2, sphereCenter);
+	if (contact.contactPointB == capsule1)
+	{
+		contact.normalB = glm::normalize(sphereCenter - capsule1);
+		contact.contactPointB = capsule1 + capsuleRadius * contact.normalB;
+	}
+	else if (contact.contactPointB == capsule2)
+	{
+		contact.normalB = glm::normalize(sphereCenter - capsule2);
+		contact.contactPointB = capsule2 + capsuleRadius * contact.normalB;
+	}
+	else
+	{
+		contact.normalB = glm::normalize(sphereCenter - contact.contactPointB);
+		contact.contactPointB = contact.contactPointB + capsuleRadius * contact.normalB;
+	}
+	contact.normalA = -contact.normalB;
+	contact.contactPointA = sphereCenter + sphereRadius * contact.normalA;
+	return contact;
 }
