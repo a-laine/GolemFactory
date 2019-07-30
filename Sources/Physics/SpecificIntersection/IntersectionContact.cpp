d533c492c6ed38b55cd211150c59fcacf352f6c0
Unknown <titwin@live.fr>
2019-07-30 20:58:12 +0200

    more implementation
    - fix missing include
    - implement sphere vs sphere
    - implement sphere vs capsule

--- Sources/Physics/SpecificIntersection/IntersectionContact.cpp
+++ Sources/Physics/SpecificIntersection/IntersectionContact.cpp
@@ -1,5 +1,7 @@
 #include "IntersectionContact.h"
 
+#include <utility>
+
 Intersection::Contact::Contact() : contactPointA(0.f), contactPointB(0.f), normalA(0.f), normalB(0.f)
 {}
 Intersection::Contact::Contact(const glm::vec3& A, const glm::vec3& B, const glm::vec3& nA, const glm::vec3& nB) : contactPointA(A), contactPointB(B), normalA(nA), normalB(nB)
