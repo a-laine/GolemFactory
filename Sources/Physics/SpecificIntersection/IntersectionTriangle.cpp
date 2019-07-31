b65b599e24ddf98802c49bfb232c91f9f3fb7a23
LAINE Thibault <titwin@live.fr>
2019-07-31 10:22:31 +0200

    more implementation
    - add key implementation : intersection point vs triangle
    - use previous key impl for triangle vs sphere
    - add use of baricentric coordinate computation function in collision point

--- Sources/Physics/SpecificIntersection/IntersectionTriangle.cpp
+++ Sources/Physics/SpecificIntersection/IntersectionTriangle.cpp
@@ -5,6 +5,7 @@
 #include "Physics/SpecificCollision/CollisionTriangle.h"
 
 #include "IntersectionSegment.h"
+#include "IntersectionPoint.h"
 
 Intersection::Contact Intersection::intersect_TrianglevsTriangle(const glm::vec3& triangle1a, const glm::vec3&triangle1b, const glm::vec3& triangle1c, const glm::vec3& triangle2a, const glm::vec3& triangle2b, const glm::vec3& triangle2c)
 {
@@ -20,40 +21,9 @@ Intersection::Contact Intersection::intersect_TrianglevsAxisAlignedBox(const glm
 }
 Intersection::Contact Intersection::intersect_TrianglevsSphere(const glm::vec3& triangle1, const glm::vec3& triangle2, const glm::vec3& triangle3, const glm::vec3& sphereCenter, const float& sphereRadius)
 {
-	return Intersection::Contact();
-	/*
-	//	initialize
-	Contact result;
-	glm::vec3 u1 = triangle2 - triangle1;
-	glm::vec3 u2 = triangle3 - triangle1;
-	result.normal1 = glm::normalize(glm::cross(u1, u2));
-	bool edgeContact = false;
-
-	//	getting closest point in triangle from barycentric coordinates
-	glm::vec2 bary = getBarycentricCoordinates(u1, u2, (sphereCenter - triangle1) - glm::dot(sphereCenter - triangle1, result.normal1) * result.normal1);
-	if (bary.x < 0.f)
-	{
-		bary.x = 0.f;
-		edgeContact = true;
-	}
-	if (bary.y < 0.f)
-	{
-		bary.y = 0.f;
-		edgeContact = true;
-	}
-	if (bary.x + bary.y > 1.f)
-	{
-		bary /= (bary.x + bary.y);
-		edgeContact = true;
-	}
-
-	result.contact1 = triangle1 + bary.x*u1 + bary.y*u2;
-	result.normal2 = glm::normalize(result.contact1 - sphereCenter);
-	result.contact2 = sphereCenter + sphereRadius * result.normal2;
-	if (edgeContact)
-		result.normal1 = -result.normal2;
-	return result;
-	*/
+	Contact contact = intersect_PointvsTriangle(sphereCenter, triangle1, triangle2, triangle3);
+	contact.contactPointA = contact.contactPointA + sphereRadius * contact.normalA;
+	return contact.swap();
 }
 Intersection::Contact Intersection::intersect_TrianglevsCapsule(const glm::vec3& triangle1, const glm::vec3& triangle2, const glm::vec3& triangle3, const glm::vec3& capsule1, const glm::vec3& capsule2, const float& capsuleRadius)
 {
