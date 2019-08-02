41ddca2636ee5a1356b8e29edf8e386ed54c779d
LAINE Thibault <titwin@live.fr>
2019-08-02 11:22:37 +0200

    more implementation
    - fix one bug in collision segment
    - impl segment vs OBB
    - impl segment vs AABB
    - finish segment vs triangle
    - impl segment vs sphere
    - impl segment capsule

--- Sources/Physics/SpecificIntersection/IntersectionSegment.cpp
+++ Sources/Physics/SpecificIntersection/IntersectionSegment.cpp
@@ -99,6 +99,8 @@ Intersection::Contact Intersection::intersect_SegmentvsTriangle(const glm::vec3&
 		else if (d2 >= d1 && d2 >= d3) return intersect_SegmentvsSegment(segment1, segment2, triangle1, triangle3);
 		else return intersect_SegmentvsSegment(segment1, segment2, triangle3, triangle2);
 	}
+
+	// eliminate collision case
 	glm::vec3 intersection;
 	if (Collision::collide_SegmentvsTriangle(segment1, segment2, triangle1, triangle2, triangle3, intersection))
 	{
@@ -138,7 +140,7 @@ Intersection::Contact Intersection::intersect_SegmentvsTriangle(const glm::vec3&
 	if(dp1 != std::numeric_limits<float>::max())
 		dp1 *= dp1;
 		
-	// compute closest segment of each edge with segment
+	// compute all segments of each edge with test segment
 	std::pair<glm::vec3, glm::vec3> s1 = getSegmentsClosestSegment(segment1, segment2, triangle1, triangle2);
 	std::pair<glm::vec3, glm::vec3> s2 = getSegmentsClosestSegment(segment1, segment2, triangle1, triangle3);
 	std::pair<glm::vec3, glm::vec3> s3 = getSegmentsClosestSegment(segment1, segment2, triangle2, triangle3);
@@ -146,56 +148,165 @@ Intersection::Contact Intersection::intersect_SegmentvsTriangle(const glm::vec3&
 	float d2 = glm::length2(s2.first - s2.second);
 	float d3 = glm::length2(s3.first - s3.second);
 
+	// choose closest point set
+	Contact contact;
 	if (dp1 < d1 && dp1 < d2 && dp1 < d3)
 	{
-
+		contact.contactPointA = pair1;
+		contact.contactPointB = p1;
 	}
+	else if (d1 < d2 && d1 < d3)
+	{
+		contact.contactPointA = s1.first;
+		contact.contactPointB = s1.second;
+	}
+	else if (d2 < d1 && d2 < d3)
+	{
+		contact.contactPointA = s2.first;
+		contact.contactPointB = s2.second;
+	}
+	else
+	{
+		contact.contactPointA = s3.first;
+		contact.contactPointB = s3.second;
+	}
+
+	// end and return
+	contact.normalA = glm::normalize(contact.contactPointB - contact.contactPointA);
+	contact.normalB = -contact.normalA;
+	return contact;
 }
 Intersection::Contact Intersection::intersect_SegmentvsOrientedBox(const glm::vec3& segment1, const glm::vec3& segment2, const glm::mat4& boxTranform, const glm::vec3& boxMin, const glm::vec3& boxMax)
 {
-	return Intersection::Contact();
+	// init
+	glm::vec3 s1 = glm::vec3(glm::inverse(boxTranform) * glm::vec4(segment1, 1.f));
+	glm::vec3 s2 = glm::vec3(glm::inverse(boxTranform) * glm::vec4(segment2, 1.f));
+	Contact contact = intersect_SegmentvsAxisAlignedBox(s1, s2, boxMin, boxMax);
+
+	// end
+	contact.contactPointA = glm::vec3(boxTranform * glm::vec4(contact.contactPointA, 1.f));
+	contact.contactPointB = glm::vec3(boxTranform * glm::vec4(contact.contactPointB, 1.f));
+	contact.normalA = glm::vec3(boxTranform * glm::vec4(contact.normalA, 0.f));
+	contact.normalB = glm::vec3(boxTranform * glm::vec4(contact.normalB, 0.f));
+	return contact;
 }
 Intersection::Contact Intersection::intersect_SegmentvsAxisAlignedBox(const glm::vec3& segment1, const glm::vec3& segment2, const glm::vec3& boxMin, const glm::vec3& boxMax)
 {
-	return Intersection::Contact();
+	if (segment2 == segment1) return intersect_PointvsAxisAlignedBox(segment1, boxMin, boxMax);
+
+	if (Collision::collide_SegmentvsAxisAlignedBox(segment1, segment2, boxMin, boxMax))
+	{
+		glm::vec3 bcenter = 0.5f * (boxMax + boxMin);
+		glm::vec3 bsize = 0.5f * glm::abs(boxMax - boxMin);
+		
+		Contact contact;
+		contact.contactPointA = getSegmentClosestPoint(segment1, segment2, bcenter);
+		glm::vec3 p = contact.contactPointA - bcenter;
+		glm::vec3 n = glm::vec3(0.f);
+
+		// inside point is closer to an x face
+		if (std::abs(p.x) > std::abs(p.y) && std::abs(p.x) > std::abs(p.z))
+		{
+			if (p.x > 0)
+			{
+				n = glm::vec3(1, 0, 0);
+				p.x = bsize.x;
+			}
+			else
+			{
+				n = glm::vec3(-1, 0, 0);
+				p.x = -bsize.x;
+			}
+		}
+
+		// inside point is closer to a y face
+		else if (std::abs(p.y) > std::abs(p.x) && std::abs(p.y) > std::abs(p.z))
+		{
+			if (p.y > 0)
+			{
+				n = glm::vec3(0, 1, 0);
+				p.y = bsize.y;
+			}
+			else
+			{
+				n = glm::vec3(0, -1, 0);
+				p.y = -bsize.y;
+			}
+		}
+
+		// inside point is closer to a z face
+		else
+		{
+			if (p.z > 0)
+			{
+				n = glm::vec3(0, 0, 1);
+				p.z = bsize.z;
+			}
+			else
+			{
+				n = glm::vec3(0, 0, -1);
+				p.z = -bsize.z;
+			}
+		}
+
+		// end and return
+		contact.contactPointB = p + bcenter;
+		contact.normalB = n;
+		contact.normalA = glm::cross(segment2 -segment1, glm::cross(n, segment2 - segment1));
+		return contact;
+	}
+	else
+	{
+		// add closest point on ABB to both segment extremities
+		std::vector<std::pair<glm::vec3, glm::vec3>> candidates;
+		Contact c = intersect_PointvsAxisAlignedBox(segment1, boxMin, boxMax);
+		candidates.push_back({ c.contactPointA, c.contactPointB });
+		c = intersect_PointvsAxisAlignedBox(segment2, boxMin, boxMax);
+		candidates.push_back({ c.contactPointA, c.contactPointB });
+
+		// adding all cube edges to candidates
+		candidates.push_back(getSegmentsClosestSegment(segment1, segment2, boxMin, glm::vec3(boxMin.x, boxMin.y, boxMax.z)));
+		candidates.push_back(getSegmentsClosestSegment(segment1, segment2, boxMin, glm::vec3(boxMin.x, boxMax.y, boxMin.z)));
+		candidates.push_back(getSegmentsClosestSegment(segment1, segment2, boxMin, glm::vec3(boxMax.x, boxMin.y, boxMin.z)));
+		candidates.push_back(getSegmentsClosestSegment(segment1, segment2, boxMax, glm::vec3(boxMax.x, boxMax.y, boxMin.x)));
+		candidates.push_back(getSegmentsClosestSegment(segment1, segment2, boxMax, glm::vec3(boxMax.x, boxMin.y, boxMax.x)));
+		candidates.push_back(getSegmentsClosestSegment(segment1, segment2, boxMax, glm::vec3(boxMin.x, boxMax.y, boxMax.x)));
+		candidates.push_back(getSegmentsClosestSegment(segment1, segment2, glm::vec3(boxMin.x, boxMax.y, boxMax.z), glm::vec3(boxMin.x, boxMin.y, boxMax.z)));
+		candidates.push_back(getSegmentsClosestSegment(segment1, segment2, glm::vec3(boxMin.x, boxMax.y, boxMax.z), glm::vec3(boxMin.x, boxMax.y, boxMin.z)));
+		candidates.push_back(getSegmentsClosestSegment(segment1, segment2, glm::vec3(boxMax.x, boxMax.y, boxMin.z), glm::vec3(boxMin.x, boxMax.y, boxMin.z)));
+		candidates.push_back(getSegmentsClosestSegment(segment1, segment2, glm::vec3(boxMax.x, boxMax.y, boxMin.z), glm::vec3(boxMax.x, boxMin.y, boxMin.z)));
+		candidates.push_back(getSegmentsClosestSegment(segment1, segment2, glm::vec3(boxMax.x, boxMin.y, boxMax.z), glm::vec3(boxMin.x, boxMin.y, boxMax.z)));
+		candidates.push_back(getSegmentsClosestSegment(segment1, segment2, glm::vec3(boxMax.x, boxMin.y, boxMax.z), glm::vec3(boxMax.x, boxMin.y, boxMin.z)));
+		
+		// sorting functions
+		auto compare = [](std::pair<glm::vec3, glm::vec3> s1, std::pair<glm::vec3, glm::vec3> s2) {
+			return glm::length2(s1.first - s1.second) < glm::length2(s2.first - s2.second);
+		};
+		std::sort(candidates.begin(), candidates.end(), compare);
+
+		// end and return
+		Contact contact;
+		contact.contactPointA = candidates.front().first;
+		contact.contactPointB = candidates.front().second;
+		contact.normalA = glm::normalize(contact.contactPointB - contact.contactPointA);
+		contact.normalB = -contact.normalA;
+		return contact;
+	}
 }
 Intersection::Contact Intersection::intersect_SegmentvsSphere(const glm::vec3& segment1, const glm::vec3& segment2, const glm::vec3& sphereCenter, const float& sphereRadius)
 {
-	return Intersection::Contact();
-	/*
 	if (segment2 == segment1) return intersect_PointvsSphere(segment1, sphereCenter, sphereRadius);
 
-	Contact result;
-	glm::vec3 s = segment2 - segment1;
-	glm::vec3 u = glm::normalize(s);
-	glm::vec3 u2 = sphereCenter - segment1;
-	result.normal2 = glm::normalize(u2 - glm::dot(u, u2) * u);
-	result.normal1 = -result.normal2;
-
-	float t = glm::clamp(glm::dot(u, u2), 0.f, glm::length(s));
-	result.contact1 = segment1 + t * u;
-	result.contact2 = sphereCenter + sphereRadius * result.normal2;
-	return result;
-	*/
+	Contact contact;
+	contact.contactPointA = getSegmentClosestPoint(segment1, segment2, sphereCenter);
+	contact.contactPointB = sphereCenter;
+	contact.normalA = glm::normalize(contact.contactPointB - contact.contactPointA);
+	contact.normalB = -contact.normalA;
+	return contact;
 }
 Intersection::Contact Intersection::intersect_SegmentvsCapsule(const glm::vec3& segment1, const glm::vec3& segment2, const glm::vec3& capsule1, const glm::vec3& capsule2, const float& capsuleRadius)
 {
-	return Intersection::Contact();
-	/*
-	glm::vec3 s1 = segment2 - segment1;
-	glm::vec3 s2 = capsule2 - capsule1;
-	if (s1 == glm::vec3(0.f))
-		return intersect_PointvsCapsule(segment1, capsule1, capsule2, capsuleRadius);
-	else if (s2 == glm::vec3(0.f))
-		return intersect_SegmentvsSphere(segment1, segment2, capsule1, capsuleRadius);
-
-	Contact result;
-	std::pair<glm::vec3, glm::vec3> closest = getSegmentsClosestSegment(segment1, segment2, capsule1, capsule2);
-	result.contact1 = closest.first;
-	result.normal2 = glm::normalize(closest.first - closest.second);
-	result.normal1 = -result.normal2;
-	result.contact2 = closest.second + capsuleRadius * result.normal2;
-
-	return result;
-	*/
+	Contact contact = intersect_SegmentvsSegment(segment1, segment2, capsule1, capsule2);
+	contact.contactPointB = contact.contactPointB + capsuleRadius * contact.normalB;
+	return contact;
 }
